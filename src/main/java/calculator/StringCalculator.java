d57a0e23fadb069881aa590da10e3b475c760d22
Snehal More <snehalmore4160@gmail.com>
2021-10-17 10:40:30 +0530

    String_delimiter_done

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,17 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	if(input.equals("")) {
         return 0;
     }
+    	else {
+    		String delimiter = ",";
+    		if(input.matches("//(.)\n(.)")) {
+    			delimiter = Character.toString(input.charAt(2));
+    			input = input.substring(4);
+    		}
+    		String numList[] = splitNumbers(input,delimiter + "|\n");
+    		return sum(numList);
+    	}
 
 }
